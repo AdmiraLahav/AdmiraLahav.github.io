5ece00377b37e4b4e9adee792544cf1327fab524
AdmiraLahav <113438937+AdmiraLahav@users.noreply.github.com>
2025-07-17 15:22:40 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
-// config.json
-const GITHUB_TOKEN = "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"; // your real token
-const GITHUB_USERNAME = "admiralahav";
-const GITHUB_REPO = "admiralahav.github.io";
-const FILE_PATH = "posts.json";
+const GITHUB_USERNAME = 'admiralahav';
+const GITHUB_REPO = 'admiralahav.github.io';
+const FILE_PATH = 'data/posts.json';
+const GITHUB_TOKEN = 'your_real_token_here'; // keep private
