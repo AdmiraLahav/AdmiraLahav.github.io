0e82a251d2134832077bcda2341589ac4f538f22
AdmiraLahav <113438937+AdmiraLahav@users.noreply.github.com>
2025-09-12 20:39:44 +0300

    Delete config.js

--- config.js
+++
@@ -1,4 +0,0 @@
-const GITHUB_USERNAME = 'admiralahav';
-const GITHUB_REPO = 'admiralahav.github.io';
-const FILE_PATH = 'data/posts.json';
-const GITHUB_TOKEN = 'your_real_token_here'; // keep private
