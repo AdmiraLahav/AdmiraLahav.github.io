2c17784362541384f009e1f52e00edc7e184941e
AdmiraLahav <113438937+AdmiraLahav@users.noreply.github.com>
2025-11-10 22:39:56 +0200

    Update script.js

--- Test/script.js
+++ Test/script.js
@@ -1,3 +1,6 @@
 document.getElementById("learnBtn").addEventListener("click", () => {
   alert("You’re officially learning web design, AdmiraLahav!");
 });
+document.getElementById("testBtn").addEventListener("hover", () +=> {
+  alert("test alert by hover");
+});
