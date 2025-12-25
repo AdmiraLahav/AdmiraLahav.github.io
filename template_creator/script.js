67198539a3e9ae31a4f0146a17988be2b409ef95
AdmiraLahav <113438937+AdmiraLahav@users.noreply.github.com>
2025-12-25 18:45:29 +0200

    Create template_creator/script.js

---
+++ template_creator/script.js
@@ -0,0 +1,25 @@
+const templates = {
+  basic: `
+# {{TITLE}}
+
+{{DESCRIPTION}}
+
+## Notes
+{{NOTES}}
+`,
+
+  cyber: `
+# {{TITLE}}
+
+{{BADGES}}
+
+## Description
+{{DESCRIPTION}}
+
+## Features
+{{FEATURES}}
+
+## Notes
+{{NOTES}}
+`
+}
