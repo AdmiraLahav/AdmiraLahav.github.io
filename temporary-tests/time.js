76cc01fe1f1ade9d2d2e7cae56c6b2ef98bc3620
AdmiraLahav <lavhrmn@gmail.com>
2026-05-23 20:05:52 +0300

    added some assets

--- temporary-tests/time.js
+++
@@ -1,9 +0,0 @@
-const date = new Date();
-let year = date.getFullYear();
-console.log(year)
-function myFunction() {
-    console.log("Running every second");
-}
-
-// Run every 1000 ms (1 second)
-setInterval(myFunction, 1000);
