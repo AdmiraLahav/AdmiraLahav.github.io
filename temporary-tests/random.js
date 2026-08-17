42ea107daa32424776140bf97212d109a1e21f19
AdmiraLahav <lavhrmn@gmail.com>
2026-08-17 14:58:14 +0300

    added more 404 pages and made them rotate between them

---
+++ temporary-tests/random.js
@@ -0,0 +1,12 @@
+a=Math.floor(Math.random() * 3);
+
+if (a==0){
+    console.warn("first")
+}
+else if (a==1){
+    console.warn("sec")
+}
+else if (a==2){
+    console.warn("third")
+}
+console.log(a)
